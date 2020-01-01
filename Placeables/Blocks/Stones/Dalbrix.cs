ce042dcc3e295241c09db4d1d909d758b634af12
charlie brannan <charliebrannan154@gmail.com>
2020-01-01 14:21:50 +1100

    changes

--- Placeables/Blocks/Stones/Dalbrix.cs
+++ Placeables/Blocks/Stones/Dalbrix.cs
@@ -1,4 +1,5 @@
 using NightmaresTMod.Tiles.Stones;
+using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -6,24 +7,17 @@ namespace NightmaresTMod.Placeables.Blocks.Stones
 {
 	public class Dalbrix : ModItem
 	{
-		public override void SetStaticDefaults()
-		{
-			DisplayName.SetDefault("Dalbrix");
-		}
-
 		public override void SetDefaults()
 		{
-			item.width = 12; // Hitbox Width
-			item.height = 12; // Hitbox Height
-			item.useTime = 10; // Speed before reuse
-			item.useAnimation = 10; // Animation Speed
-			item.useStyle = 1; // 1 = Broadsword
-			item.value = 320000; // 32 gold
-			item.rare = 7; //Lime rarity
-			item.UseSound = SoundID.Item1; // Sound effect of item on use
-			item.autoReuse = true; // Do you want to torture people with clicking? Set to false
-			item.consumable = true; // Will consume the item when placed.
-			item.maxStack = 999; // The maximum number you can have of this item.
+			item.Size = new Microsoft.Xna.Framework.Vector2(12);
+			item.useTime = item.useAnimation = 10;
+			item.useStyle = ItemUseStyleID.SwingThrow;
+			item.value = Item.sellPrice(gold: 4);
+			item.rare = ItemRarityID.Lime;
+			item.UseSound = SoundID.Item1;
+			item.autoReuse = true;
+			item.consumable = true;
+			item.maxStack = 999;
 			item.createTile = ModContent.TileType<DalbrixTile>();
 		}
 	}
